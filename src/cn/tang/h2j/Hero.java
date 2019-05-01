da423f9bc42e308bcb16aed0b39676d4a58397af
SJTang1995 <38063972+SJTang1995@users.noreply.github.com>
2019-05-01 16:45:52 +0800

    test

---
+++ src/cn/tang/h2j/Hero.java
@@ -0,0 +1,16 @@
+package cn.tang.h2j;
+
+public class Hero {
+    String name;
+    float hp;
+    float armor;
+    int moveSpeed;
+    public static void main(String args[]){
+        Hero a=new Hero();
+        a.name="tsj";
+        a.armor=100;
+        a.hp=100;
+        a.moveSpeed=100;
+
+    }
+}
---
+++ src/cn/tang/h2j/Item.java
@@ -0,0 +1,17 @@
+package cn.tang.h2j;
+
+public class Item {
+    String name;
+    int price;
+    public static void main(String Args[]){
+        Item bb=new Item();
+        Item gs=new Item();
+        Item lb=new Item();
+        lb.name="长剑";
+        lb.price=350;
+        bb.name="血瓶";
+        bb.price=50;
+        gs.name="草鞋";
+        gs.price=300;
+    }
+}
