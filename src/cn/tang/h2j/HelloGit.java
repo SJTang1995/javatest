9ffadf545f7623a2ba17d3d1197bd1ad08871776
SJTang1995 <38063972+SJTang1995@users.noreply.github.com>
2019-05-01 14:25:34 +0800

    test

---
+++ src/cn/tang/h2j/HelloGit.java
@@ -0,0 +1,9 @@
+package cn.tang.h2j;
+
+public class HelloGit {
+	public static void main(String[] args) {
+
+
+		System.out.println("Hello Git abc ");
+	}
+}
---
+++ src/cn/tang/h2j/helloworld.java
@@ -0,0 +1,8 @@
+package cn.tang.h2j;
+
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+        System.out.println("你好");
+    }
+}
